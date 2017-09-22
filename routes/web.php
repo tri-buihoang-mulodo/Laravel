f7c586c071912a6599632ba7044ce0c80649ed59
tri-buihoang-mulodo <tri.buihoang.mulodo@gmail.com>
2017-09-22 16:26:21 +0700

    Pass Data to Your Views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
+// example 01:
+// Route::get('/', function () {
+//     return view('welcome',[
+//     	'name' => 'World'
+//     ]);
+// });
+
+// example 02
+// Route::get('/', function () {
+// 	$name = 'Hoàng Trí';
+//     return view('welcome',['name' => $name]);
+// });
+
+// example 03
+// Route::get('/', function () {
+// 	$name = 'Hoàng Trí';
+// 	$age = 22;
+//     return view('welcome',compact('name','age'));
+// });
+
+// example 04
 Route::get('/', function () {
-    return view('welcome');
+	$name = 'Hoàng Trí';
+	$age = 22;
+	$tasks= [
+		'Go to the store',
+		'Finish my screencast',
+		'Clean the house'
+	];
+    return view('welcome',compact('name','age','tasks'));
 });
