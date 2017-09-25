7f8d56e403539197620bcb90966cbd17fcccd07d
tri-buihoang-mulodo <tri.buihoang.mulodo@gmail.com>
2017-09-25 15:35:51 +0700

    Route Model Binding

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,15 +6,22 @@ use App\Task;
 
 class TasksController extends Controller
 {
+    // lesson 08: Controller
     public function index()
     {
     	$tasks = Task::all();
     	return view('tasks.index',compact('tasks'));
     }
 
-    public function show($id)
+    // public function show($id)
+    // {
+    // 	$task = Task::find($id);
+    // 	return view('tasks.show',compact('task'));
+    // }
+
+    // lesson 09: Route Model Binding
+    public function show(Task $task)
     {
-    	$task = Task::find($id);
-    	return view('tasks.show',compact('task'));
+        return view('tasks.show',compact('task'));
     }
 }
