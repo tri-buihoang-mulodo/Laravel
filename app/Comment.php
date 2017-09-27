53816fe8af212d9bccfc23dd82b80537707c7d95
Bui Hoang Tri <tri.buihoang.mulodo@gmail.com>
2017-09-27 17:51:03 +0700

    Eloquent Relationships and Comments

---
+++ app/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+
+
+class Comment extends Model
+{
+    // $comment->post;
+    public function post()
+    {
+    	return $this->belongsTo(Post::class);
+    }
+}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,21 @@
 <div class="col-sm-8 blog-main">
 	<h1>{{ $post->title }}</h1>
 	{{ $post->body}}
+
+	<hr>
+
+	<div class="comments">
+		<ul class="list-group">
+			
+		</ul>
+		@foreach ($post->comments as $comment)
+		<li class="list-group-item">
+			<strong>
+				{{$comment->created_at->diffForHumans()}} : &nbsp;
+			</strong>
+			{{ $comment->body }}
+		</li>			
+		@endforeach
+	</div>
 </div>
 @endsection
