28d47b3ab98718c30d1273ea70cf8890847fd1d2
Bui Hoang Tri <tri.buihoang.mulodo@gmail.com>
2017-10-03 15:38:03 +0700

    View Composers

--- app/Post.php
+++ app/Post.php
@@ -43,4 +43,14 @@ class Post extends Model
         }
         
     }
+
+    // lesson 21
+    public static function archives()
+    {
+        return static::selectRaw('year(created_at) year,monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw('min(created_at) desc')
+            ->get()
+            ->toArray();  
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // lesson 21
+        view()->composer('layouts.sidebar', function ($view) {
+            $view->with('archives', \App\Post::archives());
+        });
     }
 
     /**
