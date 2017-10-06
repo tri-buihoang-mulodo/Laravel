4210c809e9a06a16bd16e462aaaa874bf2902207
Bui Hoang Tri <tri.buihoang.mulodo@gmail.com>
2017-10-06 17:55:59 +0700

    Form Requests and Form Objects

---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\User;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [            
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'        
+        ];
+    }
+
+    public function persist()
+    {
+        // lesson 28
+        // Create and save user
+        // $user = User::create(request(['name','email','password']));
+        $user = User::create([
+            'name' => request('name'),
+            'email'=> request('email'),
+            'password'=> bcrypt(request('password'))
+        ]);
+        
+
+        // Sign them in
+        auth()->login($user);
+
+        // lesson 26
+        \Mail::to($user)->send(new Welcome($user));
+    }
+}
